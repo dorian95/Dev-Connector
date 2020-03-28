4c4dcaf9abb967a061fe310415be664ffd62c19a
Nursultan <dorian95@uw.edu>
2020-03-27 22:27:17 -0700

    Fixed folder structure for routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,10 @@
  * */
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+
+// user model
+const User = require('../../models/User');
 
 // @route  POST api/users (api/users - endpoint)
 // @desc   Register user -send data to this route (name, email, password)
@@ -20,12 +23,32 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email }); //search by email
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get user's gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
--- server.js
+++ server.js
@@ -8,16 +8,14 @@ connectDB();
 
 // No need to instal body parser, use Init Middleware
 app.use(express.json({ extended: false }));
-//app.use(express.json()); //Used to parse JSON bodies
-//app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 //endpoint
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes, so that we can use them
-app.use('/api/users', require('./config/routes/api/users'));
-app.use('/api/auth', require('./config/routes/api/auth'));
-app.use('/api/profile', require('./config/routes/api/profile'));
-app.use('/api/posts', require('./config/routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
