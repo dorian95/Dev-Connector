d014a3f45dd64dbd94921b6c231436d3e51a783c
Nursultan <dorian95@uw.edu>
2020-06-22 22:04:18 -0700

    added alert reducer

---
+++ client/src/actions/alert.js
@@ -0,0 +1,10 @@
+import { SET_ALERT, REMOVE_ALERT } from './types';
+import uuid from 'uuid';
+
+export const setAlert = () => (dispatch) => {
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alerType, id },
+  });
+};
