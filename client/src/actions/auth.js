f60d8536649a2602c5e8a35606aaae55bc8bc941
Nursultan <dorian95@uw.edu>
2020-07-24 11:33:08 -0700

    added user login action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from './types';
 
 import setAuthToken from '../utils/setAuthToken';
@@ -31,6 +33,38 @@ export const loadUser = () => async (dispatch) => {
   }
 };
 
+// Login User Action
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    // dispatch loadUser action
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
+
 // Register User Action
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
@@ -48,6 +82,9 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
+
+    // dispatch loadUser action
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
