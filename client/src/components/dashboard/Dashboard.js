6c7eca185d1e4e9d67d9d7a7cf3307814719b5dd
Nursultan <dorian95@uw.edu>
2020-07-26 11:24:56 -0700

    added more logic for Dashboard rediredirection for for users who have & don't have a profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
@@ -20,8 +21,18 @@ const Dashboard = ({
     <Fragment>
       <h1 className='large text-primary'>Dashboard</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i>Welcome {user && user.name}{' '}
+        <i className='fas fa-user'></i>Welcome {user && user.name}
       </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' classname='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
