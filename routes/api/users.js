4641a921d414d208255cee92cfb11874ce6aa068
Nursultan <dorian95@uw.edu>
2020-09-02 21:32:17 -0700

    request body & validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route    GET api/users
-// @desc     Test route
+// @route    POST api/users
+// @desc     Register user
 // @access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email address').isEmail(),
+    check('password', 'Please enter a password with 6 or more chars').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
