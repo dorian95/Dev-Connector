80a28aa0b1f4de1f1ac843d6e7bf7760870c0cc5
Nursultan <dorian95@uw.edu>
2020-09-13 20:27:59 -0700

    user sign up: check if user alredy exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
@@ -14,12 +16,33 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists ' }] });
+      }
+
+      // Get user's gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
