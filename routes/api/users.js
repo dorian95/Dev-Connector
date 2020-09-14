907502c42f7658857c02ffef23160144d43f6ebb
Nursultan <dorian95@uw.edu>
2020-09-13 20:30:00 -0700

    get user's gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const gravatar = require('gravatar');
 const User = require('../../models/User');
 
 // @route    POST api/users
@@ -33,6 +33,11 @@ router.post(
       }
 
       // Get user's gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
       // Encrypt password
 
