97ca8ed3850750ffccac72f07e0c141962b0e3e9
Nursultan <dorian95@uw.edu>
2020-09-16 19:39:42 -0700

    added auth middleware & jwt verification

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-// MW function - has access to req/res object
-// next - callback
-module.exports = function(req, res, next) {
+// middleware function, has access to req/res objects
+// next is the callback
+module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if not token
+  // Check if no token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, auth denied' });
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   // Verify token
@@ -18,9 +18,7 @@ module.exports = function(req, res, next) {
 
     req.user = decoded.user;
     next();
-
-    // if token not valid
   } catch (err) {
-    res.status(401).json({ msg: 'Token not valid' });
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
